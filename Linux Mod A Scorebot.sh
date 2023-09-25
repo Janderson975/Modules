c3a170ddadbc728d99d38887faf666a9d54906c5
Janderson975 <131876522+Janderson975@users.noreply.github.com>
2023-09-25 08:00:31 -0400

    Update Linux Mod A Scorebot.sh

--- Linux Mod A Scorebot.sh
+++ Linux Mod A Scorebot.sh
@@ -2,21 +2,21 @@
 
 if ( eval REPLACE ); then
   if ( eval sed -n 1p README.html | grep '❌' ); then
-    sed -i '1s/❌/✅/' README.html >> README.html
+    sed -i '1s/❌/✅/' README.html
 if ( eval REPLACE ); then
   if ( eval sed -n 1p README.html | grep '❌' ); then
-    sed -i '2s/❌/✅/' > README.html >> README.html
+    sed -i '2s/❌/✅/' README.html
 if ( eval REPLACE ); then
   if ( eval sed -n 1p README.html | grep '❌' ); then
-    sed -i '3s/❌/✅/' > README.html >> README.html
+    sed -i '3s/❌/✅/' README.html
 if ( eval REPLACE ); then
   if ( eval sed -n 1p README.html | grep '❌' ); then
-    sed -i '4s/❌/✅/' > README.html >> README.html
+    sed -i '4s/❌/✅/' README.html
 if ( eval REPLACE ); then
   if ( eval sed -n 1p README.html | grep '❌' ); then
-    sed -i '5s/❌/✅/' > README.html >> README.html
+    sed -i '5s/❌/✅/' README.html
 if ( eval REPLACE ); then
   if ( eval sed -n 1p README.html | grep '❌' ); then
-    sed -i '6s/❌/✅/' >README.html >> README.html
+    sed -i '6s/❌/✅/' README.html
 
 #modify the number before s to line number of objective in html file
