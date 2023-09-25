54a7b822fb050074033bb4a0b6afa875907d8b74
Janderson975 <131876522+Janderson975@users.noreply.github.com>
2023-09-25 16:41:28 -0400

    Update Linux Mod A Scorebot.sh

--- Linux Mod A Scorebot.sh
+++ Linux Mod A Scorebot.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-if ( eval REPLACE ); then
-  if ( eval sed -n 1p README.html | grep '❌' ); then
-    sed -i '1s/❌/✅/' README.html
-if ( eval REPLACE ); then
-  if ( eval sed -n 1p README.html | grep '❌' ); then
-    sed -i '2s/❌/✅/' README.html
-if ( eval REPLACE ); then
-  if ( eval sed -n 1p README.html | grep '❌' ); then
-    sed -i '3s/❌/✅/' README.html
-if ( eval REPLACE ); then
-  if ( eval sed -n 1p README.html | grep '❌' ); then
-    sed -i '4s/❌/✅/' README.html
-if ( eval REPLACE ); then
-  if ( eval sed -n 1p README.html | grep '❌' ); then
-    sed -i '5s/❌/✅/' README.html
-if ( eval REPLACE ); then
-  if ( eval sed -n 1p README.html | grep '❌' ); then
-    sed -i '6s/❌/✅/' README.html
-
-#modify the number before s to line number of objective in html file
+if ( eval ls /home/cyber | grep Music ); then
+  if ( eval sed -n 18p README.html | grep '❌' ); then
+    sed -i '18s/❌/✅/' README.html
+if ( ! eval ls /home/cyber/Document | grep pass.txt ); then
+  if ( eval sed -n 19p README.html | grep '❌' ); then
+    sed -i '19s/❌/✅/' README.html
+if ( eval ls /home/cyber/Desktop | grep file.txt ); then
+  if ( eval sed -n 20p README.html | grep '❌' ); then
+    sed -i '20s/❌/✅/' README.html
+if ( eval cat /home/cyber/Desktop/file.txt | grep 'l0nest@r' ); then
+  if ( eval sed -n 21p README.html | grep '❌' ); then
+    sed -i '21s/❌/✅/' README.html
+if ( eval ls /home/cyber/Document | grep ubu ); then
+  if ( eval sed -n 21p README.html | grep '❌' ); then
+    sed -i '21s/❌/✅/' README.html
