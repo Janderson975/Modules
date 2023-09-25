8feae043a2fba214b12e428e5faac9dc892e2462
Janderson975 <131876522+Janderson975@users.noreply.github.com>
2023-09-25 16:57:27 -0400

    Update Linux Mod A Scorebot.sh

--- Linux Mod A Scorebot.sh
+++ Linux Mod A Scorebot.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-if ( eval ls /home/cyber | grep Music ); then
-  if ( eval sed -n 18p README.html | grep '❌' ); then
-    sed -i '18s/❌/✅/' README.html
-if ( ! eval ls /home/cyber/Document | grep pass.txt ); then
-  if ( eval sed -n 19p README.html | grep '❌' ); then
-    sed -i '19s/❌/✅/' README.html
-if ( eval ls /home/cyber/Desktop | grep file.txt ); then
-  if ( eval sed -n 20p README.html | grep '❌' ); then
-    sed -i '20s/❌/✅/' README.html
-if ( eval cat /home/cyber/Desktop/file.txt | grep 'l0nest@r' ); then
-  if ( eval sed -n 21p README.html | grep '❌' ); then
-    sed -i '21s/❌/✅/' README.html
-if ( eval ls /home/cyber/Document | grep ubu ); then
-  if ( eval sed -n 22p README.html | grep '❌' ); then
-    sed -i '22s/❌/✅/' README.html
-if ( ! eval cat /home/cyber/Desktop/README.html | grep '❌' ); then
-  sed -i '25/"<p>Congratulations! You have finished module A and now know basic terminal utility. You can now move onto Module B for basic security</p>"' README.html
+while true
+do
+  if ( eval ls /home/cyber | grep Music ); then
+    if ( eval sed -n 18p README.html | grep '❌' ); then
+      sed -i '18s/❌/✅/' README.html
+  if ( ! eval ls /home/cyber/Document | grep pass.txt ); then
+    if ( eval sed -n 19p README.html | grep '❌' ); then
+      sed -i '19s/❌/✅/' README.html
+  if ( eval ls /home/cyber/Desktop | grep file.txt ); then
+    if ( eval sed -n 20p README.html | grep '❌' ); then
+      sed -i '20s/❌/✅/' README.html
+  if ( eval cat /home/cyber/Desktop/file.txt | grep 'l0nest@r' ); then
+    if ( eval sed -n 21p README.html | grep '❌' ); then
+      sed -i '21s/❌/✅/' README.html
+  if ( eval ls /home/cyber/Document | grep ubu ); then
+    if ( eval sed -n 22p README.html | grep '❌' ); then
+      sed -i '22s/❌/✅/' README.html
+  if ( ! eval cat /home/cyber/Desktop/README.html | grep '❌' ); then
+    sed -i '25/"<p>Congratulations! You have finished module A and now know basic terminal utility. You can now move onto Module B for basic security</p>"' README.html
+done
