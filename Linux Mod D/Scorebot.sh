bac5eeae4845d0790e363c045dfe83532aa736cf
Janderson975 <131876522+Janderson975@users.noreply.github.com>
2024-03-20 11:59:18 -0400

    Update Scorebot.sh

--- Linux Mod D/Scorebot.sh
+++ Linux Mod D/Scorebot.sh
@@ -1,1 +1,22 @@
 #!/bin/bash
+
+README="/home/cyber/Desktop/README.html"
+
+function check()
+{
+  if ( eval $1 ); then
+    if ( eval sed -n $2p $README | grep '❌' ); then
+      sed -i '$2s/❌/✅/' $README
+    fi
+  fi
+  if ( ! eval $1 ); then
+    if ( eval sed -n $2p $README | grep '✅' ); then
+      sed -i '$2s/✅/❌/' $README
+    fi
+  fi
+}
+
+check 'cat /home/cyber/Desktop/README.html | grep "Linux 5.x"' '19'
+check 'cat /home/cyber/Desktop/README.html | grep "Answer" | grep "21"' '24'
+check 'cat /home/cyber/Desktop/README.html | grep "password1"' '32'
+check 'cat /home/cyber/Desktop/README.html | grep "9a900403ac313ba27a1bc81f0932652b8020dac92c234d98fa0b06bf0040ecfd"' '39'
